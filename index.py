427ecf8477e55a47ae6d251f3d5e6e30dfd13ac3
Taahia23 <taahiatahsin@gmail.com>
2025-02-21 21:49:03 +0600

    created dictionary for menu

---
+++ index.py
@@ -0,0 +1,14 @@
+menu = {
+    'Pizza' : 280,
+    'Pasta' : 180,
+    'Chawmin' : 150,
+    'Fried rice' : 190,
+    'Coffee' : 40,
+}
+
+# Greeting our customer
+print("Welcome to our restro")
+print("-------------------------------------")
+
+# Printing our menu
+print(" Pizza : 280tk\n Pasta : 180tk\n Chawmin : 150tk\n Fried rice : 190tk\n Coffee : 40tk\n")
