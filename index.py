e3b74c20e6268a2c7f5f80267a3783fd4d93e723
Taahia23 <taahiatahsin@gmail.com>
2025-02-21 22:09:51 +0600

    count total bill of ordered item

--- index.py
+++ index.py
@@ -13,12 +13,31 @@ print("-------------------------------------")
 # Printing our menu
 print(" Pizza : 280tk\n Pasta : 180tk\n Chawmin : 150tk\n Fried rice : 190tk\n Coffee : 40tk\n")
 
-# 
+# Taking first order
 
 total_order = 0
 item1 = input("Enter any item you want order: ")
 
 if item1 in menu:
-    print('Entered item is available')
+    print(f'Entered item {item1} is available')
+    total_order += menu[item1]
+    print(f'Your item {item1} has been added to your order')
 else:
-    print('Sorry! Entered item is currently unavailable')
+    print(f'Sorry! Entered item {item1} is currently unavailable')
+
+
+# Taking another order
+another_item = input('Do you want to order any other item? (Yes/No)')
+
+if another_item == 'Yes':
+    item2 = input("Enter another item you want order: ")
+    print(f'Your item {item2} has been added to your order')
+
+    if item2 in menu:
+        total_order+=menu[item2]
+        print(f'Your total bill is {total_order}')
+else: 
+    print(f'Okey! Enjoy our item!')
+    print(f'Your bill is {total_order}')
+
+
