0a3f8e779dba71473a3e3c903c848a39b8466736
Taahia23 <taahiatahsin@gmail.com>
2025-02-21 21:53:08 +0600

    checking is item is avaiable or not

--- index.py
+++ index.py
@@ -12,3 +12,13 @@ print("-------------------------------------")
 
 # Printing our menu
 print(" Pizza : 280tk\n Pasta : 180tk\n Chawmin : 150tk\n Fried rice : 190tk\n Coffee : 40tk\n")
+
+# 
+
+total_order = 0
+item1 = input("Enter any item you want order: ")
+
+if item1 in menu:
+    print('Entered item is available')
+else:
+    print('Sorry! Entered item is currently unavailable')
