105573734dc11dcf923d6a05e6b90c4ba94ec95e
Taahia23 <taahiatahsin@gmail.com>
2025-03-14 23:46:53 +0600

    added menu items

--- ui.py
+++ ui.py
@@ -1,3 +1,12 @@
 import tkinter as tk
 from tkinter import messagebox
 
+# Menu items and their prices
+menu = {
+    'Pizza': 280,
+    'Pasta': 180,
+    'Chawmin': 150,
+    'Fried Rice': 190,
+    'Coffee': 40,
+}
+
